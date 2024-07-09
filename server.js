f5d7eb0688b102ac6fbd489cd88372f6b0c988fa
Tyler Kuang <tkuang@usc.edu>
2024-07-08 23:39:27 -0700

    deploy updates

--- server.js
+++ server.js
@@ -3,14 +3,17 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+const path = require('path');
 
-// server used to send send emails
+// server used to send emails
 const app = express();
 app.use(express.static(path.resolve(__dirname, '../build')));
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server running on port 5000"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on port 5000"));
 console.log(`listening to send emails to ${process.env.EMAIL_USER}...`);
 // console.log(process.env.EMAIL_PASS);
 
