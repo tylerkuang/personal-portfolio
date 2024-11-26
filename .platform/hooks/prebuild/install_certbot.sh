77cae3bd29be1c11d5cc34873a00f01a03f904b7
Tyler Kuang <tkuang@usc.edu>
2024-11-25 16:09:06 -0800

    add platform hooks

---
+++ .platform/hooks/prebuild/install_certbot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install -y epel-release
+sudo yum install -y certbot python3-certbot-nginx
---
+++ .platform/hooks/predeploy/configure_ssl.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Obtain SSL certificate
+sudo certbot certonly --nginx -n --agree-tos --email tkuang5176@gmail.com -d tylerkuang.com -d www.tylerkuang.com
+
+# Create Nginx HTTPS configuration
+sudo tee /etc/nginx/conf.d/https.conf > /dev/null << EOL
+server {
+    listen 80;
+    server_name tylerkuang.com www.tylerkuang.com;
+    return 301 https://\$host\$request_uri;
+}
+
+server {
+    listen 443 ssl;
+    server_name tylerkuang.com www.tylerkuang.com;
+
+    ssl_certificate /etc/letsencrypt/live/tylerkuang.com/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/tylerkuang.com/privkey.pem;
+
+    ssl_protocols TLSv1.2 TLSv1.3;
+    ssl_prefer_server_ciphers on;
+    ssl_ciphers HIGH:!aNULL:!MD5;
+
+    location / {
+        proxy_pass http://127.0.0.1:8080;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+}
+EOL
+
+# Create cron job for certificate renewal
+sudo tee /etc/cron.d/certbot-renew > /dev/null << EOL
+0 0 * * * root certbot renew --quiet && systemctl reload nginx
+EOL
