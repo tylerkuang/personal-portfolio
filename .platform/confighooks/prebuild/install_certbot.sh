d5a182a2051e6a69fd43f27b8195f16852c3ec59
Tyler Kuang <tkuang@usc.edu>
2024-11-27 15:13:13 -0800

    temp changes

--- .platform/confighooks/prebuild/install_certbot.sh
+++ .platform/confighooks/prebuild/install_certbot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install -y epel-release
-sudo yum install -y certbot python3-certbot-nginx
+# sudo yum update -y
+# sudo yum install -y epel-release
+# sudo yum install -y certbot python3-certbot-nginx
